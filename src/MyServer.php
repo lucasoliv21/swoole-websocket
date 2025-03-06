7627d3d4a530f598855503ef744d20ae46a3c248
SilvioDoMine <arthurhenrique@live.com>
2025-03-06 00:01:30 -0300

    Implementação experimental de gameloop

---
+++ src/MyServer.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Swoole\Http\Request;
+use Swoole\Table;
+use Swoole\Timer;
+use Swoole\WebSocket\Frame;
+use Swoole\WebSocket\Server;
+
+class MyServer
+{
+    public function main(): void
+    {
+        $ws = new Server('0.0.0.0', 9502);
+
+        $ws->set([
+            'hook_flags' => SWOOLE_HOOK_ALL,
+        ]);
+
+        $settingsTable = new Table(1024);
+        $settingsTable->column('id', Table::TYPE_STRING, 64);
+        $settingsTable->column('status', Table::TYPE_STRING, 64);
+        $settingsTable->column('createdAt', Table::TYPE_INT);
+        $settingsTable->column('homeName', Table::TYPE_STRING, 64);
+        $settingsTable->column('homeVotes', Table::TYPE_INT);
+        $settingsTable->column('homeFlag', Table::TYPE_STRING, 256);
+        $settingsTable->column('awayName', Table::TYPE_STRING, 64);
+        $settingsTable->column('awayVotes', Table::TYPE_INT);
+        $settingsTable->column('awayFlag', Table::TYPE_STRING, 256);
+        $settingsTable->create();
+
+        $ws->on('start', function (Server $server) use ($settingsTable): void {
+            echo "server started\n";
+
+            go(function () use ($settingsTable): void {
+
+                while (true) {
+                    $settingsTable->set('game', [
+                        'id' => uniqid(),
+                        'status' => 'waiting',
+                        'homeName' => 'Real Madrid',
+                        'homeVotes' => 0,
+                        'homeFlag' => '',
+                        'awayName' => 'Barcelona',
+                        'awayVotes' => 0,
+                        'awayFlag' => '',
+                        'createdAt' => time(),
+                    ]);
+
+                    sleep(3);
+
+                    // echo "Server rolled \n";
+
+                    $settingsTable->set('game', ['status' => 'running']);
+
+                    sleep(3);
+
+                    // echo "Server finished \n";
+
+                    $settingsTable->set('game', ['status' => 'finished']);
+
+                    sleep(3);
+                }
+            });
+
+            go(function () use ($settingsTable): void {
+                Timer::tick(1000, function () use ($settingsTable): void {
+                    print_r($settingsTable->get('game'));
+                });
+            });
+        });
+
+        $ws->on('open', function (Server $server, Request $request): void {
+            echo "connection open: {$request->fd}\n";
+        });
+
+        $ws->on('message', function (Server $server, Frame $frame): void {
+            echo "received message: {$frame->data}\n";
+            $server->push($frame->fd, json_encode(["hello", "world"]));
+        });
+
+        $ws->on('close', function (Server $server, int $fd): void {
+            echo "connection close: {$fd}\n";
+        });
+
+        $ws->start();
+    }
+}
