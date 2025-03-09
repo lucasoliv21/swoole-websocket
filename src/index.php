92b55c7508a831faeb205c3efc26da50a1f42adf
SilvioDoMine <arthurhenrique@live.com>
2025-03-08 23:37:29 -0300

    Adicionado TODO

--- src/index.php
+++ src/index.php
@@ -6,3 +6,24 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $server = new App\MyServer();
 $server->main();
+
+/**
+ * @TODO - Bugfix - TOP 5 Win Rates (Média prioridade): Quando o servidor inicia,
+ * ele envia uma lista de times para o frontend de top 5, porém todos eles
+ * estão empatados com 0% de winrate, com isto, a ordem na verdade tem 
+ * a ver com a ordem que os times foram adicionados na tabela.
+ * A task é mudar esse comportamento para algo pensado: seja a lista iniciar vazia
+ * e depois ir adicionando a medida que os jogos passem, ou então fazer algo onde todos
+ * eles estão empatados em primeiro lugar... algo do tipo, sua escolha.
+ * 
+ * @TODO - Nova funcionalidade - Cronometro (Alta prioridade): Seria interessante implementar
+ * um sistema de timing de status do jogo, onde o frontend possa mostrar um cronometro
+ * ou uma barra de progresso que mostre o tempo restante para o fim da votação, ou quanto
+ * falta para iniciar um jogo. Esse temporizador precisa ser sincronizado para todos os jogadores
+ * incluindo jogadores que entram no meio do jogo. Imagine um jogador que entra no meio de
+ * uma partida do Aviator e pega a vela já em 12.51x.
+ * 
+ * @TODO - Nova funcionalidade- Últimos resultados (Baixa prioridade): Implementar uma espécie
+ * de histórico de resultados, onde o frontend possa mostrar os últimos 20 jogos que aconteceram
+ * e seus resultados pro jogador, semelhante ao histórico do Aviator ou Roleta.
+ */
