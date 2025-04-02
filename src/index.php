bc5c18f8732ca594e36808067f181c05c478c154
SilvioDoMine <arthurhenrique@live.com>
2025-04-02 19:13:25 -0300

    IDEA: Removendo ideia que já foi completada

--- src/index.php
+++ src/index.php
@@ -25,17 +25,13 @@ $server->main();
  * 
  * @TODO - Sistema de banimento e/ou suspensão de jogadores que já estão na memória.
  * 
- * @IDEA - Nova funcionalidade - Sistema de som: Sempre ter uma música de fundo tocando amena
+ * @TODO - Nova funcionalidade - Sistema de som: Sempre ter uma música de fundo tocando amena
  * e quando um jogador votar, tocar um som de chute de futebol (lembre de mudar o pitch do som a cada clique)
  * e no fim de partida/início também.
  * 
  * @IDEA - Sistema de persistência: Quando desligamos o servidor ou ao fim de cada partida,
  * nós devemos salvar o estado dos jogadores num arquivo de texto ou banco de dados externo
  * para que os dados não sejam perdidos, pois atualmente estão só em memória.
- * 
- * @IDEA - Loja de pontuação: O jogador pode trocar suas moedas por itens cosméticos para
- * personalizar sua experiência de jogo. As premições estão por definir, mas pode ser tamanho
- * do emoji de votação, cor diferenciada, maior quantidade de confetes, etc.
  * 
  * @IDEA - Nova funcionalidade - Top 10 Jogadores do dia: Ao fim do dia, os 10 jogadores
  * com mais pontos são exibidos numa lista de ranking. Pode ser uma tabela ou um modal.
